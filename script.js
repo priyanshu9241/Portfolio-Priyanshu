7a4612439dc062e4acede9f3d49b478c5688c6bc
priyanshu9241 <97443584+priyanshu9241@users.noreply.github.com>
2023-08-28 00:04:34 +0530

    add dark-mode

--- script.js
+++ script.js
@@ -34,3 +34,44 @@ var typeData = new Typed(".role", {
   backSpeed: 80,
   backDelay: 1000,
 });
+
+//DARK-MODE
+
+var darkModeToggle = document.getElementById("dark-mode-toggle");
+var darkModeToggleMob = document.getElementById("dark-mode-toggle-mob");
+var logo = document.getElementById("logo");
+var logoText = document.getElementById("logo-text");
+var hero = document.getElementById("hero");
+
+// Dark-Mode-Toggle-Large-Screens
+
+darkModeToggle.onclick = function () {
+  document.body.classList.toggle("theme");
+  document.body.classList.toggle("name");
+  document.body.classList.toggle("logo-text");
+  if (document.body.classList.contains("theme")) {
+    darkModeToggle.src = "images/userAsset/sun.png";
+    logo.src = "images/userAsset/NavLogoB.png";
+    hero.src = "images/userAsset/hero-trans.png";
+  } else {
+    darkModeToggle.src = "images/userAsset/moon.png";
+    logo.src = "images/userAsset/NavLogoW.jpg";
+    hero.src = "images/userAsset/hero.jpg";
+  }
+};
+
+// Dark-Mode-Toggle-Mobile
+darkModeToggleMob.onclick = function () {
+  document.body.classList.toggle("theme");
+  document.body.classList.toggle("name");
+  document.body.classList.toggle("logo-text");
+  if (document.body.classList.contains("theme")) {
+    darkModeToggleMob.src = "images/userAsset/sun.png";
+    logo.src = "images/userAsset/NavLogoB.png";
+    hero.src = "images/userAsset/hero-trans.png";
+  } else {
+    darkModeToggleMob.src = "images/userAsset/moon.png";
+    logo.src = "images/userAsset/NavLogoW.jpg";
+    hero.src = "images/userAsset/hero.jpg";
+  }
+};
